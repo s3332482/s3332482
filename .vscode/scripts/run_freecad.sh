7da3a5bd2c558a627791613146594fbf584f97c0
John Vial <badwolf.johnnyv@gmail.com>
2024-04-29 07:38:40 +0000

    Bringing freecade script back

--- .vscode/scripts/run_freecad.sh
+++ .vscode/scripts/run_freecad.sh
@@ -4,6 +4,7 @@ source /opt/ros/humble/setup.bash
 source install/setup.bash
 
 if [ ! -f "apps/FreeCAD.AppImage" ]; then
+    mkdir apps
     echo " ============ Downloading FreeCAD ===================="
     wget -O apps/FreeCAD.AppImage https://github.com/FreeCAD/FreeCAD/releases/download/0.21.2/FreeCAD-0.21.2-Linux-x86_64.AppImage
 
