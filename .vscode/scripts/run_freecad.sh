11530fdbd4de12e2a792cedd5d42e74c8472a45f
John Vial <badwolf.johnnyv@gmail.com>
2024-07-15 08:41:34 +0000

    Beginning adjustment for jazzy

--- .vscode/scripts/run_freecad.sh
+++ .vscode/scripts/run_freecad.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-source /opt/ros/humble/setup.bash
+source /opt/ros/jazzy/setup.bash
 source install/setup.bash
 
-if [ ! -f "apps/FreeCAD.AppImage" ]; then
-    mkdir apps
-    echo " ============ Downloading FreeCAD ===================="
-    wget -O apps/FreeCAD.AppImage https://github.com/FreeCAD/FreeCAD/releases/download/0.21.2/FreeCAD-0.21.2-Linux-x86_64.AppImage
-
-fi
-chmod +x ./apps/FreeCAD.AppImage
 echo "Starting FreeCAD, please wait ..."
-./apps/FreeCAD.AppImage --appimage-extract-and-run
+/home/ros/FreeCAD.AppImage --appimage-extract-and-run
